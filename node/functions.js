1d3be9f7b0c17220c0a4cd8d42337c1b047958ec
Joel Samaniego Campos <jsamani1@mail.sfsu.edu>
2019-02-06 20:28:11 -0800

    functions are weird in JS

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,17 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+console.log(myArrow(23));
 
 // make a function that returns a function
+
+const functionFactory = () => () => console.log('This is weird!');
+
+
+const a = functionFactory();
+const anotherFunction = functionFactory;
+
+a();
+
+anotherFunction()();
